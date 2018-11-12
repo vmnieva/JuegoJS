ce98f7dbd9e2785d3ae183e6a1c42e3d1a2afc55
Manuel Mena <manu3v@gmail.com>
2018-11-12 11:50:59 +0100

    Generacion Personajes Funcional

--- public_html/Personaje.js
+++ public_html/Personaje.js
@@ -63,9 +63,8 @@ function muertePersonaje(i) {
  * @author Manuel 
  */
 function genDivJugs(njugs) {
-    for (let index = 1; index < njugs; index++) {
-        alert(njugs);
-        var infoj = "<div id='divPj'" + index + "'> <h3>Personaje</h3>";
+    for (let index = 1; index <= njugs; index++) {
+        var infoj = "<div id='divPj" + index + "'> <h3>Personaje</h3>";
         infoj = infoj + "<img src='' alt='' id='imgPj" + index + "'>";
         infoj = infoj + "<fieldset disabled='disabled'><legend>Información</legend><label for='infoNombrePj" + index + "'>Nombre</label><input type='text' name='infoNombrePj" + index + "' id='infoNombrePj" + index + "' value=''><br><label for='infoVidaPj" + index + "'>Vida</label><input type='text' name='infoVidaPj" + index + "' id='infoVidaPj" + index + "'><br><label for='infoFuerzaPj" + index + "'>Fuerza</label><input type='text' name='infoFuerzaPj" + index + "' id='infoFuerzaPj" + index + "'><br></fieldset></div>";
         document.getElementById("contenedorpj").innerHTML += infoj;
