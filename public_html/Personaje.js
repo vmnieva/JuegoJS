175e451edf01e0f94d00b338588dfc8538cefdca
Victor Manuel Nieva Sanchez <vmanuelnieva@gmail.com>
2018-11-19 12:13:33 +0100

    DADO
    El dado ya se cambia, solo queda hacer las restas de la vida a la hora de pulsar el dado e ir poniendo a la peña en oculto cuando mueran

--- public_html/Personaje.js
+++ public_html/Personaje.js
@@ -88,26 +88,24 @@ function genDivJugs(njugs) {
 
 function realizarTirada1() {
     var n = parseInt((Math.random() * 6)+1);
-    var dado = ' ';
-    dado = "dado" + n + ".png";
-    $("dado1").attr("src", dado);
+    $("#dado1").attr("src", "Imagenes/Caras-dado/dado" + n + ".png");
     document.getElementById("tirada1").value =  n;
 }
 
 function realizarTirada2() {
     var n = parseInt((Math.random() * 6)+1);
-    $("dado2").attr("src", "dado"+n+".png");
+    $("#dado2").attr("src", "Imagenes/Caras-dado/dado"+n+".png");
     document.getElementById("tirada2").value =  n;
 }
 
 function realizarTirada3() {
     var n = parseInt((Math.random() * 6)+1);
-    $("dado3").attr("src", "dado"+n+".png");
+    $("#dado3").attr("src", "Imagenes/Caras-dado/dado"+n+".png");
     document.getElementById("tirada3").value =  n;
 }
 
 function realizarTirada4() {
     var n = parseInt((Math.random() * 6)+1);
-    $("dado4").attr("src", "dado"+n+".png");
+    $("#dado4").attr("src", "Imagenes/Caras-dado/dado"+n+".png");
     document.getElementById("tirada4").value =  n;
 }
